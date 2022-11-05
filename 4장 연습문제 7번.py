167da77a8e0cf9860620dde25ff3263548d72fa5
jinyong <112368808+ksnuKJY@users.noreply.github.com>
2022-11-05 22:00:31 +0900

    Add files via upload

---
+++ 4장 연습문제 7번.py
@@ -0,0 +1,22 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t = turtle. Turtle()
+>>> t.shape("turtle")
+>>> t.fillcolor(input("첫번째 컬러를 입력하시오.:"))
+첫번째 컬러를 입력하시오.:yellow
+>>> t.begin_fill()
+>>> t.circle(50)
+>>> t.end_fill()
+>>> t.fd(100)
+>>> t.fillcolor(input("두번째 컬러를 입력하시오.:"))
+두번째 컬러를 입력하시오.:red
+>>> t.begin_fill()
+>>> t.circle(50)
+>>> t.end_fill()
+>>> t.fd(100)
+>>> t.fillcolor(input("세번째 컬러를 입력하시오.:"))
+세번째 컬러를 입력하시오.:blue
+>>> t.begin_fill()
+>>> t.circle(50)
+>>> t.end_fill()
